ad74a4f12eb430d35f58c0cb4bb60dfc855e1ed1
gautamchandna5-alt <gautamchandna5@gmail.com>
2026-09-15 02:23:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const app = express();
 //fronted access
 
 app.use(cors({
-    origin: 'client-dashboard-frontend-33yx1qpfe-pspspsps.vercel.app', 
+    origin: 'https://client-dashboard-frontend-six.vercel.app', 
     credentials: true 
 }));
 
