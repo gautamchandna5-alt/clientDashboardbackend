9c91895c7d98ec3c67fdeb6f759a28fa894f225d
gautamchandna5-alt <gautamchandna5@gmail.com>
2026-09-24 14:58:23 +0530

    patching bugs

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { register, login, refreshToken } from '../controllers/authController.js';
+import { logout, login, refreshToken } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/refresh', refreshToken);
+router.post('/logout', logout);
 
 export default router;
